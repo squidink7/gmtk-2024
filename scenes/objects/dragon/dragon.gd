085a2feb370b1fe7d19e4eae37c9f8fe2b2f547f
sirsegv <sirsegv@sirsegv.moe>
2024-08-18 21:56:48 +0930

    fix animation for real

--- scenes/objects/dragon/dragon.gd
+++ scenes/objects/dragon/dragon.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$sprite.play('idle')
+	%fishingline.set_line_origin($primeanimpoints.get_child(0).global_position)
 
 func _process(delta: float) -> void:
 	if %fishingline.current_state == %fishingline.LineState.IDLE and is_instance_valid(%fishingline) and %fishingline.state_progress != 0:
@@ -13,8 +14,18 @@ func _process(delta: float) -> void:
 		var frame = int(%fishingline.state_progress * 3)
 		$sprite.frame = frame
 		%fishingline.set_line_origin($primeanimpoints.get_child(frame).global_position)
+
+		$hookpath/pathfollow.progress_ratio = %fishingline.state_progress
+
+	elif %fishingline.current_state == %fishingline.LineState.IDLE and %fishingline.state_progress == 0:
+		if $sprite.animation != 'idle':
+			$sprite.play('idle')
+		
+		%fishingline.set_line_origin($idleanimpoints.get_child($sprite.frame).global_position)
+		
+	
 	elif %fishingline.current_state == %fishingline.LineState.CAST:
 		if $sprite.animation != 'cast':
 			$sprite.play('cast')
-		print($sprite.frame)
+
 		%fishingline.set_line_origin($castanimpoints.get_child($sprite.frame).global_position)
