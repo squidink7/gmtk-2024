2f8e7f366a9684dafcc05d6a6252623096905430
Tabby <113022918+Joltemon@users.noreply.github.com>
2024-08-19 20:25:21 +0930

    the main menu is now functional yay

--- scenes/game/main_menu.gd
+++ scenes/game/main_menu.gd
@@ -5,3 +5,24 @@ extends Node2D
 ## Settings -> Open Settings
 ## Exit to Tile -> Open Title Screen
 ## Quit Game -> take a wild guess as to what this needs to do
+
+
+
+func _on_quit_game_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_continue_pressed() -> void:
+	get_tree().change_scene_to_file('res://scenes/game/game.tscn')
+
+
+func _on_tutorial_pressed() -> void:
+	get_tree().change_scene_to_file('res://scenes/game/tutorial.tscn')
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file('res://scenes/game/settings.tscn')
+
+
+func _on_exit_to_title_pressed() -> void:
+	get_tree().change_scene_to_file('res://scenes/game/title.tscn')
