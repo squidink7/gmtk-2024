1400a1d30e70767c067d5073e57361c04ea114d6
Tabby <113022918+Joltemon@users.noreply.github.com>
2024-08-19 19:53:47 +0930

    finished main menu layout

---
+++ scenes/game/main_menu.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+## Continue -> Hide MainMenu 
+## Tutorial -> Play Tutorial
+## Settings -> Open Settings
+## Exit to Tile -> Open Title Screen
+## Quit Game -> take a wild guess as to what this needs to do
