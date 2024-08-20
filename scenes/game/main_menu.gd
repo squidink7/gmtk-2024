45251bfea7e1f8d271e22a1b39ac4af2c9f65cbd
Tabby <113022918+Joltemon@users.noreply.github.com>
2024-08-20 22:48:07 +0930

    finished credits screen

--- scenes/game/main_menu.gd
+++ scenes/game/main_menu.gd
@@ -29,7 +29,7 @@ func _on_tutorial_pressed() -> void:
 func _on_settings_pressed() -> void:
 	$clicksound.play()
 	await get_tree().create_timer(0.5).timeout
-	get_tree().change_scene_to_file('res://scenes/game/settings.tscn')
+	get_tree().change_scene_to_file('res://scenes/game/credits.tscn')
 
 
 func _on_exit_to_title_pressed() -> void:
